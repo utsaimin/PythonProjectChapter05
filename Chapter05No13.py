b1f3157500fdd2a6e6e9736de26840a9530c5040
utsaimin <utsaimin22@gmail.com>
2020-11-30 00:25:10 +0700

    Create Chapter05No13.py

---
+++ Chapter05No13.py
@@ -0,0 +1,19 @@
+a = -4
+b = 7
+if (a>0):
+    if (b>0):
+        print("keduanya positif")
+    else:
+        print("keduanya tidak positif")
+
+
+a = -4
+b = 7
+if (a>0):
+    if (b>0):
+        print("keduanya positif")
+    else:
+        print("keduanya tidak positif")
+else:
+    print("keduanya tidak positif")
+    
