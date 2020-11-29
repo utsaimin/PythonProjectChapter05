82f4d80085bc58ffc8d9c0a3875780d96e249183
utsaimin <utsaimin22@gmail.com>
2020-11-30 00:24:32 +0700

    Create Chapter05Latihan3.py

---
+++ Chapter05Latihan3.py
@@ -0,0 +1,13 @@
+#Latihan2Chapter5
+#3
+
+j=0
+i=1
+ab=0
+while (i<=100):
+    print (i)
+    ab=ab+i
+    i=i+2
+    j=j=1
+print ('Banyaknya bilangan ganjil :'+str(j))
+print ('Banyaknya bilangan ganjil :'+str(ab))
