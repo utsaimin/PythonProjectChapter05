586a8a474d478d04792bea7f31a1bbe8f6e9060f
utsaimin <utsaimin22@gmail.com>
2020-11-30 00:25:32 +0700

    Create Latihan1Chapter5.py

---
+++ Latihan1Chapter5.py
@@ -0,0 +1,14 @@
+#Latihan1Chapter5
+#1
+
+a=int(input('Nilai Bahasa Indonesia:'))
+if(a>=0 and a<=100) :
+    b=int(input('Nilai Matematika:'))
+if(b>=0 and a<=100) :
+    c=int (input('Nilai IPA:'))
+if(c>=0 and c <=100):
+    print ('================')
+if (a>60 and b>60 and c>70):
+    print ('LULUS')
+else :
+    print ('TIDAK LULUS')
